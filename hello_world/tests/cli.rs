bb1210ffc7ae780869614b316e510bb5d1cc1057
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-05-12 23:06:02 +0000

    Simple true and false binaries

--- hello_world/tests/cli.rs
+++ hello_world/tests/cli.rs
@@ -1,8 +1,24 @@
-use std::process::Command;
+use assert_cmd::Command;
+use pretty_assertions::assert_eq;
 
 #[test]
-fn test_ls_command() {
-    let mut cmd = Command::new("ls");
-    let res = cmd.output();
-    assert!(res.is_ok());
+fn test_hello_world_command() {
+    let mut cmd = Command::cargo_bin("hello_world").unwrap();
+    let output = cmd.output().expect("fail");
+    assert!(output.status.success());
+
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stdout, "Hello, world!\n");
+}
+
+#[test]
+fn test_true_command() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn test_false_command() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
